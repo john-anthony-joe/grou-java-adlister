ddab73d82cffd972de6428af99534f80a60e6075
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2021-02-03 22:58:13 -0600

    added initial structure for sql files

--- migration.sql
+++ migration.sql
@@ -1,6 +1,7 @@
 USE adlister_db;
 
 DROP TABLE IF EXISTS ads;
+DROP TABLE IF EXISTS bats;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -11,7 +12,7 @@ CREATE TABLE users (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE ads (
+CREATE TABLE bats (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(240) NOT NULL,
@@ -20,3 +21,9 @@ CREATE TABLE ads (
     FOREIGN KEY (user_id) REFERENCES users(id)
         ON DELETE CASCADE
 );
+
+# potential add-ons for bats table: Years active, star rating
+
+# need to create categories/tags table
+
+# if we wanted to expand the project we could add tags for side characters (Joker, Robin, Catwoman, etc.)
---
+++ seeder.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (username, email, password)
+VALUES ('joe', 'josiah.thomas.cuthbert@gmail.com', 'codeup' ),
+('anthony', 'email', 'codeup' ),
+('john', 'email', 'codeup' );
+
+INSERT INTO bats (user_id, title, description)
+VALUES (1, 'The Original Batman', 'Created in 1940 by Bob Kane, this is the original batman'),
+       (2, 'Adam West\'s Batman', 'Adam West played Batman from 1966-1968 on the \'Batman\' TV Series'),
+       (3, 'Christian Bale\'s Batman', 'Christian Bale played Batman in the critically acclaimed Christopher Nolan-directed films from 2005-2012')
+
