17fa3cf6bab6dc239eebb24db4aeb162ce225b24
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2021-02-08 14:44:41 -0600

    seeder + migration

--- seeder.sql
+++ seeder.sql
@@ -4,7 +4,12 @@ VALUES ('joe', 'josiah.thomas.cuthbert@gmail.com', 'codeup' ),
 ('john', 'email', 'codeup' );
 
 INSERT INTO bats (user_id, title, image_url, start_year, end_year, description)
-VALUES (1, 'The Original Batman', 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Batman_Comic_Book_-_NARA_-_595420.gif', 1940, 2021, 'Created in 1940 by Bob Kane, this is the original Batman'),
-       (2, 'Adam West\'s Batman', 'https://media1.s-nbcnews.com/j/newscms/2017_23/2033316/170610-crop-adam-west-batman-3-ew-1156a_1c510822438ac6d3a8817026221d8f3b.fit-1240w.jpg', 1966, 1968,'Adam West\'s iconic Batman'),
-       (3, 'Christian Bale\'s Batman', 'https://static.wikia.nocookie.net/batman/images/8/8f/Christian_Bale_as_The_Dark_Knight.jpg/revision/latest?cb=20140208170841', 2005, 2012,'Christian Bale played Batman in the critically acclaimed Christopher Nolan-directed films from 2005-2012');
+VALUES (3, 'The Original Batman', 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Batman_Comic_Book_-_NARA_-_595420.gif', 1940, 2021, 'Created in 1940 by Bob Kane, this is the original Batman'),
+       (2, 'Adam West\'s Batman', 'https://media1.s-nbcnews.com/j/newscms/2017_23/2033316/170610-crop-adam-west-batman-3-ew-1156a_1c510822438ac6d3a8817026221d8f3b.fit-1240w.jpg', 1966, 1968,'Adam West\'s iconic Batman emphasized that being a good citizen is an essential part of being a hero.'),
+       (3, 'Christian Bale\'s Batman', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-2C7C3rv443RiGvnsIcWJYJGhz3Ybb31dLw&usqp=CAU', 2005, 2012,'Christian Bale played Batman in the brooding, critically acclaimed Christopher Nolan-directed films'),
+       (1, 'Lego Batman', 'https://pbs.twimg.com/profile_images/874661809139073025/X8yzIhNy_400x400.jpg', 2017, 2021,'Will Arnett voices this macho, brick-based version of the Dark Knight in the 2017 modern classic "The Lego Batman Movie.'),
+       (1, 'George Clooney\'s Batman', 'https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/30/1532429149-1e980862f8bdcb640629e067a84caee3.jpg', 1997, 1997,'George Clooney played Batman in the rather unfortunate "Batman & Robin". Boasting an 11% score on Rotten Tomatoes, it may be in the best interest of fans to forget about this one.'),
+       (2, 'Val Kilmer\'s Batman', 'https://d.newsweek.com/en/full/1524146/val-kilmer-batman.jpg', 1995, 1995,'Kilmer played Batman only once, in the Tim Burton produced "Batman Forever"'),
+       (1, 'Ben Affleck\'s Batman', 'https://media.vanityfair.com/photos/5f36ca61f51ed31274cdfb62/1:1/w_1079,h_1079,c_limit/batman-ben-affleck.jpg', 2016, 2017,'Often referred to as "Batfleck", Ben Affleck\'s gruff take on the masked vigilante was not particularly well-received in spite of his impressive acting chops'),
+       (1, 'Robert Pattinson\'s Batman', 'https://i.redd.it/wt2siwq09hw51.jpg', 2022, 2022,'After Affleck\'s Batman was met with disappointment, the franchise is looking to the future with RPat. I, for one, am excited');
 
